e7460c2b242568155f0dda6958218e8490348fea
pass-pass-pass <102561261+pass-pass-pass@users.noreply.github.com>
2022-11-12 11:57:06 -0600

    Add files via upload

--- src/board.cpp
+++ src/board.cpp
@@ -7,10 +7,51 @@ Board::Board(unsigned n) {
 }
 
 void Board::toggle(unsigned row, unsigned col) {
-    assert(row < size && col < size && row >= 0 && col >= 0);
-    for (unsigned i = row; i < row + 3; i++) {
-        for (unsigned j=  col; j < col + 3; j++) {
-            _board[row][col] = !_board[row][col];
+    
+    if (!(row <= size - 3 && col <= size - 3 && row >= 0 && col >= 0)) {
+        throw runtime_error("wrong  s size");
+    }
+
+    for (unsigned i = row; i < row + 3 ; i++) {
+        for (unsigned j=  col; j   < col + 3; j++) {
+            _board[i][j] = !_board[i][j];
+        }
+    }
+}
+
+std::string Board::print() const {
+    string toReturn;
+    for (unsigned i = 0; i < size; i++) {
+        for (unsigned j =0; j < size; j++) {
+            if (_board[i][j]) {
+                toReturn.append("O");
+            } else {
+                toReturn.append("#");
+            }
+        }
+        if (i != size - 1) {
+            toReturn.append("\n");
+        }
+    }
+    return toReturn;
+ }
+
+Board::Board(const Board& rhs) {
+    size = rhs.size;
+    _board.resize(size, vector<bool>(size, false));
+    for (unsigned i = 0; i < size; i++) {
+        for (unsigned j =0; j < size; j++) {
+            _board[i][j] = rhs._board[i][j];
+        }
+    }
+}
+
+Board::Board(vector<vector<bool>> board) {
+    size = board.size();
+    _board.resize(size, vector<bool>(size, false));
+    for (unsigned i = 0 ; i < size; i++) {
+        for (unsigned j =0; j < size; j++) {
+            _board[i][j] = board[i][j];
         }
     }
 }
@@ -56,12 +97,15 @@ Board& Board::operator=(const Board& rhs) {
 }
 
 inline bool operator==(const Board& lhs, const Board& rhs) {
-    if (lhs.size != rhs.size) {
-        throw runtime_error("wrong size");
+    int a = lhs.getSize();
+    int b = rhs.getSize();
+    if (!(a == b)) {
+        throw runtime_error("wrong  size");
     }
+    int tem = lhs.getSize();
     unsigned size = lhs.getSize();
-    for (unsigned i = 0; i < size; i++) {
-        for (unsigned j =0; j < size; j++) {
+    for (int i = 0; i < tem; i++) {
+        for (int j = 0; j < tem; j++) {
             if (lhs.getBulb(i,j) != rhs.getBulb(i,j)) {
                 return false;
             }
@@ -69,6 +113,7 @@ inline bool operator==(const Board& lhs, const Board& rhs) {
     }
     return true;
 }
+
 
 inline bool operator!=(const Board& lhs, const Board& rhs) {
 
