c22d87481bff3737542e263b8956c5feebb631b8
pass-pass-pass <102561261+pass-pass-pass@users.noreply.github.com>
2022-11-11 23:33:13 -0600

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -11,10 +11,10 @@
 using namespace cs225;
 using namespace std;
 
-
 TEST_CASE("Constructor", "[weight=10][part1]")
 {
 	Board board(12);
-	
+	Board board1(0);
+	Board board2(3);
 	SUCCEED("Yep the constructor worked");
 }
