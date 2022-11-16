40ffa1b5f2edf68d9c0504721bee8eef74b0201b
Heaper327 <jameszhu327@outlook.com>
2022-11-16 23:59:03 +0000

    Replace Node class with Compare class

--- src/solver.h
+++ src/solver.h
@@ -3,6 +3,7 @@
 #include <stdexcept>
 #include <queue>
 #include <map>
+#include <algorithm>
 
 #include "board.h"
 
@@ -12,93 +13,119 @@
 */
 class Solver {
     public:
-        /**
-         * Default constructor that creates a 5x5 game whose initial state is all off
-         * and whose goal state is all on
-        */
-        Solver() = delete;
-        /**
-         * Parametrized constructor that loads a game from a formatted text file
-         * The first row of the file will contain n, the size of the board
-         * The next 2n rows will contain the initial board and the goal board, where
-         * 0 represents an off light bulb and 1 represents an on light bulb
-         * 
-         * e.g.
-         * 3
-         * 000
-         * 000
-         * 000
-         * 111
-         * 111
-         * 111
-         * 
-         * An exception will be thrown if n < 3, the boards are malformed (incorrect number of rows or columns),
-         * or invalid characters are found
-        */
-        Solver(string filename);
-        /**
-         * Parametrized constructor that loads a game from a given initial and goal board
-         * 
-         * Throws an exception if the two boards are of different dimensions
-        */
-        Solver(const Board& intial, const Board& goal);
-        /**
-         * Copy constructor that creates a deep copy of another game
-        */
-        Solver(const Solver&);
-        /**
-         * Copy assignment operator that creates a deep copy of another game
-        */
-        Solver& operator=(const Solver&);
-        /**
-         * Default destructor
-        */
-        ~Solver() = default;
-        /**
-         * Finds a solution to the game - a finite sequence of toggles that transforms the initial board
-         * to the goal board - using A* search algorithm
-         * 
-         * @return A list of boards, each representing a step in the solution
-        */
-        list<Board> solveAStar() const;
+    /**
+     * Default constructor that creates a 5x5 game whose initial state is all off
+     * and whose goal state is all on
+    */
+    Solver() = delete;
+    /**
+     * Parametrized constructor that loads a game from a formatted text file
+     * The first row of the file will contain n, the size of the board
+     * The next 2n rows will contain the initial board and the goal board, where
+     * 0 represents an off light bulb and 1 represents an on light bulb
+     * 
+     * e.g.
+     * 3
+     * 000
+     * 000
+     * 000
+     * 111
+     * 111
+     * 111
+     * 
+     * An exception will be thrown if n < 3, the boards are malformed (incorrect number of rows or columns),
+     * or invalid characters are found
+    */
+    Solver(string filename);
+    /**
+     * Parametrized constructor that loads a game from a given initial and goal board
+     * 
+     * Throws an exception if the two boards are of different dimensions
+    */
+    Solver(const Board& intial, const Board& goal);
+    /**
+     * Copy constructor that creates a deep copy of another game
+    */
+    Solver(const Solver&);
+    /**
+     * Copy assignment operator that creates a deep copy of another game
+    */
+    Solver& operator=(const Solver&);
+    /**
+     * Default destructor
+    */
+    ~Solver() = default;
+    /**
+     * Finds a solution to the game - a finite sequence of toggles that transforms the initial board
+     * to the goal board - using A* search algorithm
+     * 
+     * @return A list of boards, each representing a step in the solution
+    */
+    list<Board> solveAStar() const;
+
     private:
+    /**
+     * The initial and goal board of the game
+    */
+    Board _initial;
+    Board _goal;
+
+    // TODO: calculating heuristics takes O(n^2) time, should we memoize it instead?
+    /**
+     * Comparator classed used by the priority queue of the A* solve method
+    */
+    class Compare{
+        public:
         /**
-         * The initial and goal board of the game
+         * Parametrized constructor
         */
+        Compare(const Board& initial, const Board& goal);
+        /**
+         * Compares the priority of two boards in regard to searching for the goal board
+         * More specifically, this function compares d(a) + h(a) with d(b) + h(b),
+         * where d is the distance of a board to the initial board, and h is the heuristic function
+         * 
+         * @return true if a should be explored before b, and false otherwise
+        */
+        bool operator()(const Board& a, const Board& b) const;
+        /**
+         * Try to update the distance to origin of to_update by going through shortcut
+         * Assumes that shortcut is a neighbor of to_update
+         * 
+         * @param to_update The board whose distance to origin is to be updated
+         * @param shortcut The board that we try to travel through to get to to_update
+         * @return true if to_update's distance is reduced, false otherwise
+        */
+        bool updateDist(const Board& to_update, const Board& shortcut);
+
+        private:
+        /**
+         * Distance map and goal board must be stored in compare
+         * so they can be accessed by the comparator
+        */
+        map<Board, unsigned> dist_to_initial;
         Board _initial;
         Board _goal;
+    };
 
-        struct Node{
-            Board board;
-            unsigned dist_to_initial;
-            unsigned dist_to_goal;
-            /**
-             * Comparator used in the priority queue of the A* solve method
-             * The comparator assumes that both a and b are in _dist_to_initial
-             * 
-             * @return true if a should be visited before b, and false otherwise
-            */
-            bool operator<(const Node& rhs) const;
-        };
-
-        /**
-         * Return the hamming distance between two boards a and b. The hamming distance is defined
-         * As the number of light bulbs whose state differ between the two boards
-         * 
-         * e.g. The hamming distance between the following two boards is 2
-         * 000 110
-         * 111 111
-         * 000 000
-         * 
-         * @return The estimated distance
-        */
-        unsigned hammingDist(const Board& a, const Board& b) const;
-        /**
-         * Private helper that find all neighbors of a board. That is, all boards that can be reached
-         * by performing one toggle operation on the given board
-         * 
-         * @param board the board whose neighbors are to be found
-         * @return a list of boards that are neighbors to the given board
-        */
-        list<Board> getNeighbors(const Board& board) const;
+    /**
+     * Return the hamming distance between two boards a and b. The hamming distance is defined
+     * As the number of light bulbs whose state differ between the two boards
+     * 
+     * e.g. The hamming distance between the following two boards is 2
+     * 000 110
+     * 111 111
+     * 000 000
+     * 
+     * @return The estimated distance
+    */
+    static unsigned hammingDist(const Board& a, const Board& b);
+    /**
+     * Private helper that find all neighbors of a board. That is, all boards that can be reached
+     * by performing one toggle operation on the given board
+     * 
+     * @param board the board whose neighbors are to be found
+     * @return a list of boards that are neighbors to the given board
+    */
+    static list<Board> getNeighbors(const Board& board);
 };
