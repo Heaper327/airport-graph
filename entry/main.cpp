03e9a8da48cb201c7682ab006e8375d813a3ce22
Heaper327 <jameszhu327@outlook.com>
2022-11-18 04:21:07 +0000

    Run solver and visualize solution in main

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include "board.h"
+#include "solver.h"
 #include "cs225/PNG.h"
 
 using namespace std;
 
 int main()
 {
-    Board b(10);
-    b.toggle(3, 3);
+    Board initial(5);
+    Board goal = initial;
+    goal.toggle(0, 0);
+
+    Solver solver{initial, goal};
+    auto solution = solver.solveAStar();
+    for (const auto& step : solution) {
+        cout << "==========\n";
+        cout << step.print() << "\n";
+    }
     return 0;
 }
